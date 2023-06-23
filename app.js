fdcfdd019efd7e3fb04f57f52fc28aeb913fc7d0
Rami KHADDOUR <ramikhaddour@gmail.com>
2023-06-23 08:49:46 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/* Step à réaliser pour les cookies
+* 1) Récupérer les inputs
+* 2) Ajouter les évènements correspondant pour le formulaire 
+* 3) Créer un objet cookie contenant les valeurs des champs input + une date d'expiration
+* 
+*/
+
